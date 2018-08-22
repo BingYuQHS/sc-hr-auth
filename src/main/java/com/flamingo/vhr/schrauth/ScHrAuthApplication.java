0b7ba57f85d66a4019a724dfc9ef3f23364bfcd1
秦华山 <1756217895@qq.com>
2018-08-22 15:59:55 +0800

    initial commit

---
+++ src/main/java/com/flamingo/vhr/schrauth/ScHrAuthApplication.java
@@ -0,0 +1,12 @@
+package com.flamingo.vhr.schrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScHrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScHrAuthApplication.class, args);
+	}
+}
